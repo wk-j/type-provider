b067863bb174a95f66150fb639109b16b03e2cfb
wk-j <somnuk.wk@gmail.com>
2017-12-10 01:53:10 +0700

    Added dependencies

--- src/HelloTypeProvider/Program.fs
+++ src/HelloTypeProvider/Program.fs
@@ -1,6 +1,29 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open ProviderImplementation
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+
+[<TypeProvider>]
+type BasicProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces (config)
+
+    let ns = "StaticProperty.Provided"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let createTypes () =
+        let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
+        let myProp = ProvidedProperty("MyProperty", typeof<string>, isStatic = true, getterCode = (fun args -> <@@ "Hello world" @@>))
+        myType.AddMember(myProp)
+        [myType]
+
+    do
+        this.AddNamespace(ns, createTypes())
+
+[<assembly:TypeProviderAssembly>]
+do ()
 
 [<EntryPoint>]
 let main argv =
